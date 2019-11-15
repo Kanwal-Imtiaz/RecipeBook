937b1814e65211c06af27b42bfceb976c971e86c
Kanwal-Itmiaz <kanwalimtiaz94@gmail.com>
2019-11-15 14:25:19 +1100

    Updates

---
+++ precache-manifest.a1ef295edce7dcd0075fb3ac03f62957.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0471215a9d23ff4ddec4edb9608ae9e4",
+    "url": "/RecipeBook/index.html"
+  },
+  {
+    "revision": "8b06d773c56e2a57dfe5",
+    "url": "/RecipeBook/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7cd948c0d70438c769d6",
+    "url": "/RecipeBook/static/css/main.c4f006a3.chunk.css"
+  },
+  {
+    "revision": "8b06d773c56e2a57dfe5",
+    "url": "/RecipeBook/static/js/2.0e678b1c.chunk.js"
+  },
+  {
+    "revision": "7cd948c0d70438c769d6",
+    "url": "/RecipeBook/static/js/main.441aed27.chunk.js"
+  },
+  {
+    "revision": "52b00a87f64b4254f007",
+    "url": "/RecipeBook/static/js/runtime-main.5d3cea77.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RecipeBook/static/media/SEGA.12a3db8b.woff"
+  }
+]);
